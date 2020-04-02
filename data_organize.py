33f15364702f5349fd3c358177f41c8dcd31a82f
joncheryl <prof.sherrill@gmail.com>
2020-04-02 12:05:19 -0600

    dealt with expections in nytimes data (NYC and KC)

--- data_organize.py
+++ data_organize.py
@@ -1,27 +1,28 @@
-#
+###
 # Putting all the data together
-#
+###
 
 import pandas as pd
 
+###
+# Data for density
+###
+
+# land area by county
 area_data = pd.read_csv("county_area.csv", dtype={'state': 'string',
                                                   'county': 'string'})
-
 area_data['fips'] = area_data.state + area_data.county
 
+# population by county
 pop_data = pd.read_csv("co-est2019-alldata.csv", encoding='latin-1',
+                       usecols={'STATE', 'COUNTY', 'POPESTIMATE2019'},
                        dtype={'STATE': 'string', 'COUNTY': 'string'})
-
-pop_data = pop_data[['STATE', 'COUNTY', 'POPESTIMATE2019']]
-
-pop_data['fips'] = pop_data.STATE + pop_data.COUNTY
-
 pop_data = pop_data.rename(columns={'STATE': 'state',
                                     'COUNTY': 'county',
                                     'POPESTIMATE2019': 'population'})
+pop_data['fips'] = pop_data.state + pop_data.county
 
-# fucking manually enter Guam, PR, Virgin Islands, Mariana, American Samoa
-
+# Join area and population tables and get density
 all_data = area_data.merge(pop_data)
 
 # convert square meters to square miles
@@ -30,8 +31,11 @@ all_data.area /= 2589988
 # compute population density for each county
 all_data['density'] = all_data.population / all_data.area
 
-# manually add shutdown data
-# Date shelter-in-place order was put in place for each state
+###
+# Data for dates by states
+###
+
+# shelter-in-place order was put in place by state
 shutdown = [('01', ''),
             ('02', '2020-03-28'),
             ('04', '2020-03-31'),
@@ -87,23 +91,42 @@ shutdown = [('01', ''),
 
 sd = pd.DataFrame(shutdown, columns=['state', 'date_shutdown'], dtype='string')
 
-# get time when cases in state surpassed N
+# date when cases in each state surpassed N
 state_cases = pd.read_csv('https://raw.githubusercontent.com/nytimes/'
                           'covid-19-data/master/us-states.csv',
                           dtype={'fips': 'string'})
 N = 10
 states = state_cases.fips.unique()
-sd['date_surpassed'] = ''
+sd['date_surpass'] = ''
 
 for s in states:
     truth = (state_cases.fips == s) & (state_cases.cases > N)
     if sum(truth) > 0:
-        sd.loc[sd.state == s, 'date_surpassed'] = min(state_cases[truth].date)
+        sd.loc[sd.state == s, 'date_surpass'] = min(state_cases[truth].date)
 
 all_data = all_data.merge(sd)
 
+
 # drop columns we no longer need
-# all_data = all_data.drop(columns=['state', 'county', 'area', 'population'])
 all_data = all_data.drop(columns=['area', 'county', 'population'])
 
-all_data.to_csv('all_data.csv', index=False)
+#
+# manually enter data for NYC and Kansas City
+#
+nyc_data = pd.DataFrame({'state': ['36'],
+                         'name': ['New York City'],
+                         'fips': ['36999'],
+                         'density': [28188],
+                         'date_shutdown': sd[sd.state == '36'].date_shutdown,
+                         'date_surpass': sd[sd.state == '36'].date_surpass})
+
+kc_data = pd.DataFrame({'state': ['29'],
+                        'name': ['Kansas City'],
+                        'fips': ['29999'],
+                        'density': [1400],
+                        'date_shutdown': sd[sd.state == '29'].date_shutdown,
+                        'date_surpass': sd[sd.state == '29'].date_surpass})
+
+all_data = pd.concat([all_data, nyc_data, kc_data], ignore_index=True)
+
+all_data.to_csv('county_data.csv', index=False)
